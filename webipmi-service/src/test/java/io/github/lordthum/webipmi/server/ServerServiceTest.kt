6efb57ba10754431663704384c08651ede8a256b
Benjamin Thum <thb2dr@bosch.com>
2026-04-13 15:11:02 +0200

    create mock test

---
+++ webipmi-service/src/test/java/io/github/lordthum/webipmi/server/ServerServiceTest.kt
@@ -0,0 +1,13 @@
+package io.github.lordthum.webipmi.server
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class ServerServiceTest {
+    @Test
+    fun getById() {
+        Assertions.assertTrue(true)
+    }
+
+}
